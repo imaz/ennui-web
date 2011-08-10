7593c1e8d4b358034656cea6dc4dbdc34f473bd8
imaz <nekol.daisuki.070109@gmail.com>
2011-08-10 13:54:07 +0900

    hit/blow実装、答え＋入力値＋チェック結果を表示するところまで 残：logの蓄積、終了時処理

--- js/codebreaker.js
+++ js/codebreaker.js
@@ -2,6 +2,13 @@ var answerCode;
 function setText(id,str){
   document.getElementById(id).innerHTML = str;
 }
+function getText(id){
+  return(window.document.getElementById(id).innerHTML;
+}
+function textColoring(str,color){
+  var retText = "\<font style=\"color:"+color+"\"\>"+str+"\</font\>";
+  return(retText);
+}
 function getRadioValue(radioObject){
   for(i=0; i<radioObject.length; i++){
     if(radioObject[i].checked){
@@ -10,13 +17,41 @@ function getRadioValue(radioObject){
   }
   return("(´・ω・｀)");
 }
+function getCode(){
+  var code = new Array(4);
+  code[0] = getRadioValue(window.document.inputForm.radio1);
+  code[1] = getRadioValue(window.document.inputForm.radio2);
+  code[2] = getRadioValue(window.document.inputForm.radio3);
+  code[3] = getRadioValue(window.document.inputForm.radio4);
+  return code;
+}
+function hit(code){
+  var hit = 0;
+  for(i=0;i<4;i++){
+    if(code[i] == answerCode[i]){
+      code[i] = 'x';
+      hit++;
+    }
+  }
+  return(hit);
+}
+function blow(code){
+  var blow = 0;
+  for(i=0;i<4;i++){
+    for(y=0;y<4;y++){
+      if(code[i] == answerCode[i]){
+        code[i] = 'x';
+        blow++;
+        break;
+      }
+    }
+  }
+  return(blow);
+}
 function checkCode(){
-  var code;
-  code = getRadioValue(window.document.inputForm.radio1);
-  code += getRadioValue(window.document.inputForm.radio2);
-  code += getRadioValue(window.document.inputForm.radio3);
-  code += getRadioValue(window.document.inputForm.radio4);
-  setText("codeLog",code);
+  code = getCode();
+  setText("codeLog",answerCode+" "+code);
+  setText("codeLog",getText("codeLog")+" H"+hit(code)+" B"+blow(code));
 }
 function createCode(){
   answerCode = new Array(4);
@@ -24,5 +59,4 @@ function createCode(){
   answerCode[1] = Math.floor(Math.random()*10);
   answerCode[2] = Math.floor(Math.random()*10);
   answerCode[3] = Math.floor(Math.random()*10);
-  console.log(answerCode);
 }
