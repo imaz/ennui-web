1c6a9042e234a6cc691ca5f3e991b1034e0855e0
imaz <nekol.daisuki.070109@gmail.com>
2011-08-11 09:44:02 +0900

    * answerCodeに同じ数字が2回入らないように変更 * [bug]同じ数字が2回入っていた時に正確にblowを検出できない

--- js/codebreaker.js
+++ js/codebreaker.js
@@ -1,14 +1,22 @@
 var answerCode;
+
+Array.prototype.clone = function(){
+  return Array.apply(null,this)
+}
+
 function setText(id,str){
   document.getElementById(id).innerHTML = str;
 }
+
 function getText(id){
   return(window.document.getElementById(id).innerHTML;
 }
+
 function textColoring(str,color){
   var retText = "\<font style=\"color:"+color+"\"\>"+str+"\</font\>";
   return(retText);
 }
+
 function getRadioValue(radioObject){
   for(i=0; i<radioObject.length; i++){
     if(radioObject[i].checked){
@@ -17,6 +25,7 @@ function getRadioValue(radioObject){
   }
   return("(´・ω・｀)");
 }
+
 function getCode(){
   var code = new Array(4);
   code[0] = getRadioValue(window.document.inputForm.radio1);
@@ -25,22 +34,26 @@ function getCode(){
   code[3] = getRadioValue(window.document.inputForm.radio4);
   return code;
 }
-function hit(code){
+
+function hit(ans,code){
   var hit = 0;
   for(i=0;i<4;i++){
-    if(code[i] == answerCode[i]){
+    if(code[i] == ans[i]){
       code[i] = 'x';
+      ans[y] = 'y';
       hit++;
     }
   }
   return(hit);
 }
-function blow(code){
+
+function blow(ans,code){
   var blow = 0;
   for(i=0;i<4;i++){
     for(y=0;y<4;y++){
-      if(code[i] == answerCode[i]){
+      if(code[i] == ans[y]){
         code[i] = 'x';
+        ans[y] = 'y';
         blow++;
         break;
       }
@@ -48,15 +61,35 @@ function blow(code){
   }
   return(blow);
 }
+
 function checkCode(){
   code = getCode();
-  setText("codeLog",answerCode+" "+code);
-  setText("codeLog",getText("codeLog")+" H"+hit(code)+" B"+blow(code));
+  ans = answerCode.clone();
+  setText("codeLog",getText("codeLog")+"\<br /\>");
+  setText("codeLog",getText("codeLog")+" "+code);
+  setText("codeLog",getText("codeLog")+" H"+hit(ans,code)+" B"+blow(ans,code));
 }
-function createCode(){
+
+function initializeCode(){
   answerCode = new Array(4);
-  answerCode[0] = Math.floor(Math.random()*10);
-  answerCode[1] = Math.floor(Math.random()*10);
-  answerCode[2] = Math.floor(Math.random()*10);
-  answerCode[3] = Math.floor(Math.random()*10);
+  i=0;
+  while(i!=4){
+    c = 0;
+    c = Math.floor(Math.random()*10);
+    if(Array.indexOf(answerCode,c) == -1){
+      answerCode[i] = c;
+      i++;
+    }
+  }
+  // setText("debug",answerCode);
+}
+
+function createCode(){
+  initializeCode();
+}
+
+function reset(){
+  //これが呼ばれていない感じ
+  initializeCode();
+  setText("codeLog","");
 }
